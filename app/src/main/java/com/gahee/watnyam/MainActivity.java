41219ecc7e31d6066164be4c47233326b59b2cfc
한가희 <asgard774@hanyang.ac.kr>
2019-12-28 15:23:49 +0900

    Initial commit

---
+++ app/src/main/java/com/gahee/watnyam/MainActivity.java
@@ -0,0 +1,37 @@
+package com.gahee.watnyam;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.gahee.watnyam.ui.fragments.MyFragPagerAdapter;
+import com.gahee.watnyam.ui.fragments.RouletteFragment;
+import com.gahee.watnyam.ui.fragments.WorldCupFragment;
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity
+        implements RouletteFragment.OnFragmentInteractionListener
+                , WorldCupFragment.OnFragmentInteractionListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MyFragPagerAdapter myFragPagerAdapter = new MyFragPagerAdapter(getSupportFragmentManager());
+
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(myFragPagerAdapter);
+
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
+
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
