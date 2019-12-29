37d67c6f02cf5f851b2fc4ff428803ce09025c7b
한가희 <asgard774@hanyang.ac.kr>
2019-12-29 10:45:19 +0900

    -

---
+++ app/src/main/java/com/forif/watnyam/Config.java
@@ -0,0 +1,12 @@
+package com.forif.watnyam;
+
+public class Config {
+
+    public static final String GOOGLE_API_KEY = "AIzaSyCr8rQBPZMtDFZTuW0dLySE51RWP1rxpHA";
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCgo7BMFB1zgOgmrKU4MWn9zu8jjfJrwhI";
+
+    public static final String GOOGLE_SEARCH_ENGINE_ID = "014695961139869825366:pt17zwd5wo2";
+
+//
+}
